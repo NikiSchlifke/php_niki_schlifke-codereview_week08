02bfeb5daaa35b14163e9c60ea621ff99e9b5853
Nikolaus Schlifke <nikolaus.schlifke@kouba.at>
2017-05-10 21:50:36 +0200

    refactor pages

--- initdb.php
+++ initdb.php
@@ -25,3 +25,34 @@ SELECT id FROM product WHERE uuid = :uuid;
 SQL
 );
 
+$addBillSQL = $dbh->prepare(<<<'SQL'
+INSERT INTO bill (credit_card_number, payment_status, cart_id) VALUES (:creditCardNumber, 'unverified', :cartId)
+;
+SQL
+);
+
+
+$addCartSQL = $dbh->prepare(<<<'SQL'
+INSERT INTO cart (customer_id, bill_id) VALUES (:customerId, :billId)
+;
+SQL
+);
+
+$addCartCompositionSQL = $dbh->prepare(<<<'SQL'
+INSERT INTO cart_composition (product_id, product_count, cart_id) VALUES (:productId, :productCount, :cartId)
+;
+SQL
+);
+
+$addRegionSQL = $dbh->prepare(<<<'SQL'
+INSERT INTO region (nation, country, city, zip) VALUES (:nation, :country, :city, :zip)
+;
+SQL
+);
+
+
+$addCustomerSQL = $dbh->prepare(<<<'SQL'
+INSERT INTO customer (address_id, first_name, last_name) VALUES (:addressId, :firstName, :lastName)
+;
+SQL
+);
