4ed9b3f159b03b27ef33bbf12a3a3376f07bad02
Nikolaus Schlifke <nikolaus.schlifke@kouba.at>
2017-05-10 21:47:56 +0200

    refactor pages

--- index.php
+++ index.php
@@ -22,30 +22,9 @@
 
         </div>
         <div class="col-md-4 text-center" id="product-cart">
-            <div class="row">
-                <div class="col-xs-12" id="product-cart-items">
+            <?php require_once 'includes/shoppingCartItems.php'?>
 
-                </div>
-            </div>
-
-                    <div class="card" id="cart-total-card">
-                        <div class="card-block">
-                            <h1 class="card-title">Cart Totals</h1>
-                            <div class="row">
-                                <div class="col-xs-12 col-md-6 card-text">Subtotal</div>
-                                <div class="col-xs-12 col-md-6 card-text" id="total-price">$ 0.00</div>
-                            </div>
-                            <hr>
-                            <div class="row">
-                                <div class="col-xs-12 col-md-6 card-text">Shipping:</div>
-                                <div class="col-xs-12 col-md-6 card-text" id="total-shipping">Free (Free)</div>
-                            </div>
-                        </div>
-                        <div class="card-block">
-                            <a href="confirmPayment.php" class="btn btn-success checkout-buttons">PROCEED TO CHECKOUT</a>
-                        </div>
-
-                    </div>
+            <?php require_once 'includes/shoppingCartTotal.php'?>
 
 
         </div>
